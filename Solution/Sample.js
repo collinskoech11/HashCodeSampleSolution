f3c326f8f431df79560213c32250fdc8661c32aa
collinskoech11 <collinskoechck34@gmail.com>
2021-02-25 17:58:40 +0300

    update

--- Solution/Sample.js
+++ Solution/Sample.js
@@ -1,4 +1,17 @@
 var m = require('m');
 var readline = require(readline);
 
-const filenames = ["a_example","b_little_bit_of_everything","c_many_ingredients","d_many_pizzas","e_many_teams"]
+const fileNames = ["a_example","b_little_bit_of_everything","c_many_ingredients","d_many_pizzas","e_many_teams"];
+
+fileNames.forEach((fileName) => {
+
+    var lineReader = readline.createInterface({
+        input: m.createReadStream('./Input/${filename}.in')
+    });
+
+    let data = [];
+
+    lineReader.on('line', function(line) {
+        data.push(line.split(" "))
+    });
+})
